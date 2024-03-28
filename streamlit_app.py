4fcc1cfd35261d4c8e412244c5b4ebb99cb434df
dpiron <den.piron@gmail.com>
2024-03-28 10:22:52 +0100

    EMA Crossover

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,140 +5,193 @@ import matplotlib.pyplot as plt
 import ta
 import streamlit as st
 
-profit_losses = None
-
-
+# Function to fetch historical stock price data using Yahoo Finance API
 def fetch_stock_data(ticker, start_date, end_date):
-    """Fetch historical stock price data using Yahoo Finance API."""
     data = yf.download(ticker, start=start_date, end=end_date)
     return data
 
 
-def calculate_technical_indicators(data):
-    """Calculate technical indicators including RSI, Stochastic Oscillator, Williams %R, 50-day MA, and 200-day MA."""
-    data['RSI'] = ta.momentum.RSIIndicator(data['Close'], window=14).rsi()
-    data['%K'] = ta.momentum.StochasticOscillator(data['High'], data['Low'], data['Close'], window=14,
-                                                  smooth_window=3).stoch()
-    data['Williams_R'] = ta.momentum.WilliamsRIndicator(data['High'], data['Low'], data['Close'], lbp=5).williams_r()
+def calculate_technical_indicators(data, short_term_ema_days=14, long_term_ema_days=200):
+    data['Short_EMA'] = data['Close'].ewm(span=short_term_ema_days, adjust=False).mean()
+    data['Long_EMA'] = data['Close'].ewm(span=long_term_ema_days, adjust=False).mean()
 
-    # Calculate 50-day and 200-day moving averages
-    data['50_MA'] = data['Close'].rolling(window=50).mean()
-    data['200_MA'] = data['Close'].rolling(window=200).mean()
+    # Calculate RSI
+    data['RSI'] = ta.momentum.RSIIndicator(data['Close'], window=14).rsi()
 
     return data
 
 
-def backtest_strategy(data):
-    """Backtest the trading strategy with modified entry and exit rules."""
-    global profit_losses
-    profit_losses = np.full(len(data), np.nan)
+# Entry conditions based on EMAs and RSI
+def entry_condition(data, current_index):
+    if current_index < 1:
+        return False
+    return (data['Short_EMA'].iloc[current_index - 1] < data['Long_EMA'].iloc[current_index - 1]) and \
+        (data['Short_EMA'].iloc[current_index] > data['Long_EMA'].iloc[current_index])
+            #and \
+            #(data['RSI'].iloc[current_index] >= 50) and (data['RSI'].iloc[current_index] <= 70))
 
+
+# Exit conditions based on RSI and profit
+def exit_condition(data, current_index):
+    if current_index < 1:
+        return False
+    return (data['Short_EMA'].iloc[current_index - 1] > data['Long_EMA'].iloc[current_index - 1]) and \
+        (data['Short_EMA'].iloc[current_index] < data['Long_EMA'].iloc[current_index])
+            #and \
+            #(data['RSI'].iloc[current_index] <= 50) and (data['RSI'].iloc[current_index] >= 30)
+
+# Backtest strategy
+def backtest_strategy(data, entry_condition, exit_condition, commission, initial_investment=400):
     position = None
     buy_price = None
     accumulated_profit = 0
     buy_markers = []
     sell_markers = []
+    cumulative_profit = []
+
+    shares_bought = 0  # Initialize shares_bought outside the loop
 
     for index, row in data.iterrows():
         close_price = row['Close']
-        williams_r = row['Williams_R']
-        prev_williams_r = data.loc[index - pd.Timedelta(days=1), 'Williams_R'] if index - pd.Timedelta(
-            days=1) in data.index else None
+        current_index = data.index.get_loc(index)  # Get the current index position
 
-        # Check for downtrend (50-day MA below 200-day MA)
-        if row['50_MA'] < row['200_MA']:
-            if position is not None:
-                # Generate a sell signal if already in position
-                profit_loss = close_price - buy_price
-                accumulated_profit += profit_loss
-                profit_losses[data.index.get_loc(index)] = accumulated_profit
-                position = None
-                sell_markers.append(index)
-            continue  # Skip trading further until a buy signal
+        # Check entry condition
+        if entry_condition(data, current_index) and position is None:
+            position = index
+            buy_price = close_price
+            buy_markers.append(index)
+            shares_bought = initial_investment / buy_price
 
-        if williams_r > -80 and prev_williams_r is not None and prev_williams_r <= -80:
-            if position is None and index in data.index:
-                position = index
-                buy_price = close_price
-                buy_markers.append(index)
+        # Check exit condition
+        if exit_condition(data, current_index) and position is not None:
+            sell_price = close_price
+            profit_loss = (sell_price - buy_price) * shares_bought - commission  # Account for commission
+            #if profit_loss > 0:  # Sell only if profit is positive
+            accumulated_profit += profit_loss
+            position = None
+            sell_markers.append(index)
 
-        if williams_r < -20 and prev_williams_r is not None and prev_williams_r >= -20:
-            if position is not None:
-                profit_loss = close_price - buy_price
-                accumulated_profit += profit_loss
-                profit_losses[data.index.get_loc(index)] = accumulated_profit
-                position = None
-                sell_markers.append(index)
+        cumulative_profit.append(accumulated_profit)
 
-    profit_losses = pd.Series(profit_losses).fillna(method='ffill').values
-    first_sell_index = data.index.get_loc(sell_markers[0]) if sell_markers else len(data)
-    profit_losses[:first_sell_index] = 0
-
-    return buy_markers, sell_markers
+    return buy_markers, sell_markers, cumulative_profit
 
 
 
-def plot_results(data, buy_markers, sell_markers, start_date, end_date):
-    """Plot stock price and cumulative profit/loss with buy/sell labels."""
-    global profit_losses
-    fig, ax1 = plt.subplots(figsize=(20, 12))  # Increased figure size
 
-    # Filter data within the specified date range
-    filtered_data = data.loc[start_date:end_date]
-
-    ax1.plot(filtered_data.index, filtered_data['Close'], label='Close Price', color='blue')
-    ax1.plot(filtered_data.index, filtered_data['50_MA'], label='50-day MA', color='orange')
-    ax1.plot(filtered_data.index, filtered_data['200_MA'], label='200-day MA', color='green')
-
-    # Fill between 50-day and 200-day MAs
-    ax1.fill_between(filtered_data.index, filtered_data['50_MA'], filtered_data['200_MA'],
-                     where=filtered_data['50_MA'] < filtered_data['200_MA'], color='red', alpha=0.3)
-
-    ax1.set_ylabel('Price', fontsize=16)  # Increased font size
-    ax1.set_xlabel('Date', fontsize=16)  # Increased font size
-
-    buy_markers_filtered = [marker for marker in buy_markers if marker in filtered_data.index]
-    ax1.scatter(buy_markers_filtered, filtered_data['Close'].loc[buy_markers_filtered], color='green', marker='^',
-                label='Buy', s=100)  # Increased marker size
-
-    sell_markers_filtered = [marker for marker in sell_markers if marker in filtered_data.index]
-    ax1.scatter(sell_markers_filtered, filtered_data['Close'].loc[sell_markers_filtered], color='red', marker='v',
-                label='Sell', s=100)  # Increased marker size
-
-    if profit_losses is not None and len(profit_losses) == len(data.index):
-        ax2 = ax1.twinx()
-        ax2.step(data.index, profit_losses, where='post', label='Profit/Loss',color='orange')
-        ax2.set_ylabel('Profit/Loss', fontsize=16)  # Increased font size
-
-    fig.tight_layout()
-    fig.legend(loc="upper left", bbox_to_anchor=(0.1, 0.9), fontsize=14)  # Increased font size
-
-    # Increase tick label sizes
-    ax1.tick_params(axis='both', which='major', labelsize=14)
-    if 'ax2' in locals():
-        ax2.tick_params(axis='both', which='major', labelsize=14)
-
-    return fig
-
-
-if __name__ == "__main__":
-    st.set_page_config(layout="wide", page_title='Stock Analysis', page_icon=":chart_with_upwards_trend:",
-                       initial_sidebar_state="expanded")
+# Streamlit app
+def main():
+    st.set_page_config(layout="wide")
 
     st.title('Stock Analysis')
 
-    # Add input box for ticker symbol
-    ticker = st.sidebar.text_input('Enter Ticker Symbol', 'GOOG')
+    tickers = st.sidebar.text_input('Enter Tickers (comma-separated)',
+                                    'SE,NU,ANNX,HEIA.AS,ADBE,FSLR,AMAT,ADSK,GOOG,PYPL,MSFT,AMD,NVDA')
+    tickers = [ticker.strip() for ticker in tickers.split(',')]
 
-    st.sidebar.title('Graph Settings')
-    start_date_graph = st.sidebar.date_input('Start Date', pd.to_datetime('2014-01-01'),
-                                             min_value=pd.to_datetime('2004-01-01'), max_value=pd.to_datetime('today'))
-    end_date_graph = st.sidebar.date_input('End Date', pd.to_datetime('2024-01-01'))
+    start_date = pd.Timestamp('2014-01-01')
+    end_date = pd.Timestamp('2024-01-01')
 
-    data = fetch_stock_data(ticker, start_date_graph, end_date_graph)
-    data = calculate_technical_indicators(data)
-    buy_markers, sell_markers = backtest_strategy(data)
-    selected_data = data.loc[start_date_graph:end_date_graph]
+    st.sidebar.subheader('Select Parameters')
+    short_term_ema_days = st.sidebar.number_input('Short Term EMA (days)', value=14, step=1)
+    long_term_ema_days = st.sidebar.number_input('Long Term EMA (days)', value=200, step=1)
+    commission = st.sidebar.number_input('Commission', value=10.0, step=1.0)
+    initial_investment = st.sidebar.number_input('Initial Investment', value=400, step=1)
 
-    fig = plot_results(data, buy_markers, sell_markers, start_date_graph, end_date_graph)
+    dataframes = []
+    buy_markers_list = []
+    sell_markers_list = []
+    cumulative_profit_list = []
+
+    for ticker in tickers:
+        data = fetch_stock_data(ticker, start_date, end_date)
+        data = calculate_technical_indicators(data, short_term_ema_days, long_term_ema_days)
+
+        # Backtest strategy based on EMAs and RSI
+        buy_markers, sell_markers, cumulative_profit = backtest_strategy(data, entry_condition, exit_condition,
+                                                                         commission, initial_investment)
+        dataframes.append(data)
+        buy_markers_list.append(buy_markers)
+        sell_markers_list.append(sell_markers)
+        cumulative_profit_list.append(cumulative_profit)
+
+    # Plot results for each ticker
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    for i, (dataframe, ticker) in enumerate(zip(dataframes, tickers)):
+        color = plt.cm.tab10(i)  # Choose a color from the tab10 colormap
+        linestyle = '-'  # Use a solid line style
+        linewidth = 0.5  # Set line width to be thinner
+        ax1.plot(dataframe.index, dataframe['Close'], label=f'{ticker} - Close Price', color=color, linestyle=linestyle,
+                 linewidth=linewidth)
+        ax1.plot(dataframe.index, dataframe['Short_EMA'], label=f'{ticker} - Short EMA', color=color, linestyle='--',
+                 linewidth=linewidth)
+        ax1.plot(dataframe.index, dataframe['Long_EMA'], label=f'{ticker} - Long EMA', color=color, linestyle=':',
+                 linewidth=linewidth)
+
+    ax1.set_ylabel('Price', fontsize=14)
+    ax2 = ax1.twinx()
+
+    for i, cumulative_profit in enumerate(cumulative_profit_list):
+        color = plt.cm.tab10(i)  # Choose a color from the tab10 colormap
+        linestyle = '-'  # Use a solid line style
+        linewidth = 0.5  # Set line width to be thinner
+        ax2.step(dataframes[i].index, cumulative_profit, label=f'{tickers[i]} - Cumulated Profit', color=color,
+                 linestyle=linestyle, linewidth=linewidth)
+
+    ax2.set_ylabel('Cumulated Profit', fontsize=14)
+    ax1.set_title('Stock Prices and Cumulated Profits', fontsize=16)
+    # ax1.legend(loc='upper left', fontsize=10)
+    # ax2.legend(loc='upper right', fontsize=10)
+    ax1.grid(True)
+    ax2.grid(False)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    plt.tight_layout()
+
     st.pyplot(fig)
+
+    # Generate summary table
+    table_data = []
+    total_trades = 0
+    total_avg_trades_per_week = 0
+    total_num_wins = 0
+    total_total_profit = 0
+    total_avg_profit_per_month = 0
+    total_avg_profit_per_trade = 0
+
+    for ticker, cumulative_profit, sell_markers in zip(tickers, cumulative_profit_list, sell_markers_list):
+        trades = len(sell_markers)
+        avg_trades_per_week = trades / ((end_date - start_date).days / 7)
+        num_wins = sum(1 for i in range(1, len(cumulative_profit)) if cumulative_profit[i] > cumulative_profit[i - 1])
+        if cumulative_profit:
+            total_profit = cumulative_profit[-1]
+        else:
+            total_profit = 0
+
+        avg_profit_per_month = total_profit / (
+                    (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month))
+        avg_profit_per_trade = total_profit / trades if trades > 0 else 0  # Avoid division by zero
+        total_trades += trades
+        total_avg_trades_per_week += avg_trades_per_week
+        total_num_wins += num_wins
+        total_total_profit += total_profit
+        total_avg_profit_per_month += avg_profit_per_month
+        total_avg_profit_per_trade += avg_profit_per_trade
+
+        table_data.append((ticker, trades, avg_trades_per_week, num_wins, total_profit, avg_profit_per_month,
+                           round(avg_profit_per_trade, 2)))
+
+    # Add row for total
+    total_row = ('Total', total_trades, total_avg_trades_per_week, total_num_wins, total_total_profit,
+                 total_avg_profit_per_month, round(total_avg_profit_per_trade, 2))
+    table_data.append(total_row)
+
+    # Display summary table
+    st.subheader('Summary Table')
+    df = pd.DataFrame(table_data,
+                      columns=['Ticker', 'Number of Trades', 'Avg Trades/Week', 'Number of Wins', 'Total Profit',
+                               'Avg Profit/Month', 'Avg Profit/Trade'])
+    st.table(df)
+
+if __name__ == "__main__":
+    main()
