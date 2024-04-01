6cf987ef1b2f75ca1666e732d7fb22ec56132eb9
dpiron <den.piron@gmail.com>
2024-04-01 09:39:53 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import yfinance as yf
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import ta
 import streamlit as st
+
 
 # Function to fetch historical stock price data using Yahoo Finance API
 def fetch_stock_data(ticker, start_date, end_date):
@@ -11,37 +11,60 @@ def fetch_stock_data(ticker, start_date, end_date):
     return data
 
 
-def calculate_technical_indicators(data, short_term_ema_days=100, long_term_ema_days=200):
+def calculate_technical_indicators(data, short_term_ema_days=100, long_term_ema_days=200, rsi_days=2):
     data['Short_EMA'] = data['Close'].ewm(span=short_term_ema_days, adjust=False).mean()
     data['Long_EMA'] = data['Close'].ewm(span=long_term_ema_days, adjust=False).mean()
 
     # Calculate RSI
-    data['RSI'] = ta.momentum.RSIIndicator(data['Close'], window=14).rsi()
+    data['RSI'] = ta.momentum.RSIIndicator(data['Close'], window=rsi_days).rsi()
 
     return data
 
 
 # Entry conditions based on EMAs and RSI
-def entry_condition(data, current_index):
+def entry_condition(data, current_index, buy_uptrend, buy_cross_over, buy_rsi, buy_rsi_value):
     if current_index < 1:
         return False
-    return (data['Short_EMA'].iloc[current_index - 1] < data['Long_EMA'].iloc[current_index - 1]) and \
-        (data['Short_EMA'].iloc[current_index] > data['Long_EMA'].iloc[current_index])
-            #and \
-            #(data['RSI'].iloc[current_index] >= 50) and (data['RSI'].iloc[current_index] <= 70))
+    if buy_uptrend:
+        is_uptrend = data['Short_EMA'].iloc[current_index] >= data['Long_EMA'].iloc[current_index]
+    else:
+        is_uptrend = True
+    if buy_cross_over:
+        is_cross_over = (data['Short_EMA'].iloc[current_index - 1] <= data['Long_EMA'].iloc[current_index - 1]) and \
+                        (data['Short_EMA'].iloc[current_index] > data['Long_EMA'].iloc[current_index])
+    else:
+        is_cross_over = False
+    if buy_rsi:
+        is_rsi = (data['RSI'].iloc[current_index - 1] >= buy_rsi_value) and \
+                 (data['RSI'].iloc[current_index] < buy_rsi_value)
+    else:
+        is_rsi = False
+    return is_uptrend and (is_cross_over or is_rsi)
 
 
 # Exit conditions based on RSI and profit
-def exit_condition(data, current_index):
+def exit_condition(data, current_index, sell_cross_over, sell_rsi, sell_rsi_value, sell_days, sell_after_days):
     if current_index < 1:
         return False
-    return (data['Short_EMA'].iloc[current_index - 1] > data['Long_EMA'].iloc[current_index - 1]) and \
-        (data['Short_EMA'].iloc[current_index] < data['Long_EMA'].iloc[current_index])
-            #and \
-            #(data['RSI'].iloc[current_index] <= 50) and (data['RSI'].iloc[current_index] >= 30)
+    if sell_days and (current_index >= sell_after_days):
+        return True
+    if sell_cross_over:
+        is_cross_over = (data['Short_EMA'].iloc[current_index - 1] >= data['Long_EMA'].iloc[current_index - 1]) and \
+                        (data['Short_EMA'].iloc[current_index] < data['Long_EMA'].iloc[current_index])
+    else:
+        is_cross_over = False
+    if sell_rsi:
+        is_rsi = (data['RSI'].iloc[current_index - 1] <= sell_rsi_value) and \
+                 (data['RSI'].iloc[current_index] > sell_rsi_value)
+    else:
+        is_rsi = False
+    return is_cross_over or is_rsi
+
 
 # Backtest strategy
-def backtest_strategy(data, entry_condition, exit_condition, commission, initial_investment=400):
+def backtest_strategy(data, entry_condition, exit_condition, commission, initial_investment,
+                      buy_uptrend, buy_cross_over, buy_rsi, buy_rsi_value,
+                      sell_cross_over, sell_rsi, sell_rsi_value, sell_days, sell_after_days):
     position = None
     buy_price = None
     accumulated_profit = 0
@@ -56,17 +79,18 @@ def backtest_strategy(data, entry_condition, exit_condition, commission, initial
         current_index = data.index.get_loc(index)  # Get the current index position
 
         # Check entry condition
-        if entry_condition(data, current_index) and position is None:
+        if entry_condition(data, current_index,
+                           buy_uptrend, buy_cross_over, buy_rsi, buy_rsi_value) and position is None:
             position = index
             buy_price = close_price
             buy_markers.append(index)
             shares_bought = initial_investment / buy_price
 
         # Check exit condition
-        if exit_condition(data, current_index) and position is not None:
+        if exit_condition(data, current_index,
+                          sell_cross_over, sell_rsi, sell_rsi_value, sell_days, sell_after_days) and position is not None:
             sell_price = close_price
             profit_loss = (sell_price - buy_price) * shares_bought - commission  # Account for commission
-            #if profit_loss > 0:  # Sell only if profit is positive
             accumulated_profit += profit_loss
             position = None
             sell_markers.append(index)
@@ -74,8 +98,6 @@ def backtest_strategy(data, entry_condition, exit_condition, commission, initial
         cumulative_profit.append(accumulated_profit)
 
     return buy_markers, sell_markers, cumulative_profit
-
-
 
 
 def main():
@@ -91,10 +113,44 @@ def main():
     end_date = pd.Timestamp('2024-01-01')
 
     st.sidebar.subheader('Select Parameters')
-    short_term_ema_days = st.sidebar.number_input('Short Term EMA (days)', value=100, step=1)
-    long_term_ema_days = st.sidebar.number_input('Long Term EMA (days)', value=200, step=1)
-    commission = st.sidebar.number_input('Commission', value=10.0, step=1.0)
     initial_investment = st.sidebar.number_input('Initial Investment', value=400, step=1)
+    commission = st.sidebar.number_input('Commission', value=10.0, step=1.0)
+
+    long_term_ema_days = st.sidebar.number_input('Long Term EMA (days)', value=200, step=1)
+    short_term_ema_days = st.sidebar.number_input('Short Term EMA (days)', value=100, step=1)
+    rsi_days = st.sidebar.number_input('RSI (days)', value=2, step=1)
+
+    st.sidebar.subheader('Entry Conditions')
+    buy_uptrend = st.sidebar.checkbox('Short EMA need to be above Long EMA', key='price_EMA')
+    buy_cross_over = st.sidebar.checkbox('Buy at EMA cross-over')
+    buy_rsi = st.sidebar.checkbox('Buy if RSI < value')
+    if buy_rsi:
+        buy_rsi_value = st.sidebar.number_input('RSI <', 0, 100, 10, 1)
+    else:
+        buy_rsi_value = 0
+
+    st.sidebar.subheader('Exit Conditions')
+    sell_cross_over = st.sidebar.checkbox('Sell at EMA cross-over')
+    sell_rsi = st.sidebar.checkbox('Sell if RSI > value')
+    if sell_rsi:
+        sell_rsi_value = st.sidebar.number_input('RSI >', 0, 100, 60, 1)
+    else:
+        sell_rsi_value = 0
+    sell_days = st.sidebar.checkbox('Sell after days')
+    if sell_days:
+        sell_after_days = st.sidebar.number_input('Days', 0, 500, 10, 1)
+    else:
+        sell_after_days = 0
+
+    # entry :
+    # buy if ema long above price? AND
+
+    # buy at cross-over? OR
+    # buy if rsi under x?
+
+    # exit :
+    # sell at cross-over? OR
+    # sell if rsi above y?
 
     dataframes = []
     buy_markers_list = []
@@ -105,24 +161,19 @@ def main():
 
     for ticker in tickers:
         data = fetch_stock_data(ticker, start_date, end_date)
-        data = calculate_technical_indicators(data, short_term_ema_days, long_term_ema_days)
+        data = calculate_technical_indicators(data, short_term_ema_days, long_term_ema_days, rsi_days)
 
         # Backtest strategy based on EMAs and RSI
         buy_markers, sell_markers, cumulative_profit = backtest_strategy(data, entry_condition, exit_condition,
-                                                                         commission, initial_investment)
+                                                                         commission, initial_investment,
+                                                                         buy_uptrend, buy_cross_over, buy_rsi,
+                                                                         buy_rsi_value,
+                                                                         sell_cross_over, sell_rsi, sell_rsi_value,
+                                                                         sell_days, sell_after_days)
         dataframes.append(data)
         buy_markers_list.append(buy_markers)
         sell_markers_list.append(sell_markers)
         cumulative_profit_list.append(cumulative_profit)
-
-        last_date = data.index[-1]
-        new_buy_signal = len(buy_markers) > 0 and buy_markers[-1] == last_date
-        new_sell_signal = len(sell_markers) > 0 and sell_markers[-1] == last_date
-
-        new_signals[ticker] = {
-            'Buy': new_buy_signal,
-            'Sell': new_sell_signal
-        }
 
     # Plot results for each ticker
     fig, ax1 = plt.subplots(figsize=(10, 6))
@@ -169,7 +220,8 @@ def main():
     total_avg_profit_per_month = 0
     total_avg_profit_per_trade = 0
 
-    for ticker, cumulative_profit, sell_markers, buy_markers in zip(tickers, cumulative_profit_list, sell_markers_list, buy_markers_list):
+    for ticker, cumulative_profit, sell_markers, buy_markers in zip(tickers, cumulative_profit_list, sell_markers_list,
+                                                                    buy_markers_list):
         trades = len(sell_markers)
         avg_trades_per_week = trades / ((end_date - start_date).days / 7)
         num_wins = sum(1 for i in range(1, len(cumulative_profit)) if cumulative_profit[i] > cumulative_profit[i - 1])
@@ -179,7 +231,7 @@ def main():
             total_profit = 0
 
         avg_profit_per_month = total_profit / (
-                    (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month))
+                (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month))
         avg_profit_per_trade = total_profit / trades if trades > 0 else 0  # Avoid division by zero
         total_trades += trades
         total_avg_trades_per_week += avg_trades_per_week
@@ -202,59 +254,6 @@ def main():
                       columns=['Ticker', 'Number of Trades', 'Avg Trades/Week', 'Number of Wins', 'Total Profit',
                                'Avg Profit/Month', 'Avg Profit/Trade'])
     st.table(df)
-
-    # Display new signals and open positions
-    st.subheader('New Signals and Open Positions')
-
-    # Create a list to store the data
-    table_data = []
-
-    for i, (ticker, signals) in enumerate(new_signals.items()):
-        signal_status = ''
-        if signals['Buy'] and signals['Sell']:
-            signal_status = 'Both buy and sell signals'
-        elif signals['Buy']:
-            signal_status = 'Buy signal'
-        elif signals['Sell']:
-            signal_status = 'Sell signal'
-        else:
-            signal_status = 'No new buy or sell signals'
-
-        # Check if there is an open position
-        buy_markers = buy_markers_list[i]
-        sell_markers = sell_markers_list[i]
-        if len(buy_markers) > len(sell_markers):
-            last_buy_date = buy_markers[-1]
-            last_buy_price = dataframes[i].loc[last_buy_date, 'Close']
-            current_close_price = dataframes[i].iloc[-1]['Close']
-            shares_bought = initial_investment // last_buy_price  # Using integer division to get whole number of shares
-            current_potential_profit = (current_close_price - last_buy_price) * shares_bought
-
-            # Add data to the table
-            table_data.append({
-                'Ticker': ticker,
-                'Signal': signal_status,
-                'Open Position': True,
-                'Buy Price': round(last_buy_price),
-                'Last Close Price': round(current_close_price),
-                'Current Potential Profit if Sold Today': round(current_potential_profit)
-            })
-        else:
-            # Add data to the table
-            table_data.append({
-                'Ticker': ticker,
-                'Signal': signal_status,
-                'Open Position': False,
-                'Buy Price': '',
-                'Last Close Price': '',
-                'Current Potential Profit if Sold Today': ''
-            })
-
-    # Convert the list of dictionaries to a DataFrame
-    table_df = pd.DataFrame(table_data)
-
-    # Display the table
-    st.table(table_df)
 
 
 if __name__ == "__main__":
